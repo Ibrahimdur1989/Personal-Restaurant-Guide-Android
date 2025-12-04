d1ac060ec91ca34fcdaf04fbdaea51f48ddc4d3e
tanupreetkaur <tanubal21@gmail.com>
2025-12-04 12:01:11 -0500

    Added database, repository and view model for Room db - part 2

--- app/src/main/java/com/example/project_g4_personalrestaurantguide/roomDb/AppDatabase.java
+++ app/src/main/java/com/example/project_g4_personalrestaurantguide/roomDb/AppDatabase.java
@@ -1,4 +1,26 @@
 package com.example.project_g4_personalrestaurantguide.roomDb;
 
-public class AppDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Restaurant.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase instance;
+
+    public abstract RestaurantDao restaurantDao();
+
+    public static synchronized AppDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    AppDatabase.class,
+                    "restaurant_database")
+                    .build();
+        }
+        return instance;
+    }
 }
